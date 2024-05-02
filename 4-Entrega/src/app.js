64af8d496099a4564e9f870a4c2b8ecbc46da3a4
Luis Velasquez <luisfer2432@gmail.com>
2024-05-01 23:41:39 -0600

    Agregando Entrega 4

---
+++ 4-Entrega/src/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const productsRoutes = require("./routes/products.routes.js");
+const cartsRoutes = require("./routes/carts.routes.js");
+const viewRoutes = require("./routes/views.routes.js");
+
+PORT = 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+app.use("/api/products", productsRoutes);
+app.use("/api/carts", cartsRoutes);
+app.use("/", viewRoutes);
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+const io = require("socket.io")(server);
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", path.join(`${__dirname}/views`));
+app.set("view engine", "handlebars");
+app.set("io", io);
---
+++ 4-Entrega/src/routes/views.routes.js
@@ -0,0 +1,41 @@
+const { Router } = require("express");
+const path = require("path");
+const pathDB = path.join(__dirname, "../data/products.json");
+const styles = path.join(`${__dirname}/../public/styles/styles.css`);
+const ProductManager = require("../managers/ProductManager.js");
+const products = new ProductManager(pathDB);
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  products
+    .getProducts()
+    .then((result) => {
+      res.render("index", {
+        title: "PreEntrega4-Backend",
+        products: result,
+        style: "styles.css",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err.message);
+    });
+});
+
+router.get("/realtimeproducts", (req, res) => {
+  products
+    .getProducts()
+    .then((result) => {
+      res.render("realtimeproducts", {
+        title: "Productos en tiempo real",
+        products: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err.message);
+    });
+});
+
+module.exports = router;
